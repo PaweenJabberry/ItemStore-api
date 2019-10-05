c53dd23b621a8eeae8954b78c65dbd41353e0363
JabBerry <33551495+PaweenJabberry@users.noreply.github.com>
2019-10-05 12:37:54 +0700

    Initial commit

---
+++ src/main/java/com/ItemStore/api/ItemStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.ItemStore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemStoreApiApplication.class, args);
+	}
+
+}
